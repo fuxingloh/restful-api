a5a4b3d02ab62bbf0ac4390c9f16caab9d273269
Fuxing Loh <lohfuxing@gmail.com>
2018-05-17 01:41:00 +0800

    Added ability to add static Exception handling to ExceptionParser

--- restful-client/src/main/java/munch/restful/client/ExceptionParser.java
+++ restful-client/src/main/java/munch/restful/client/ExceptionParser.java
@@ -1,12 +1,17 @@
 package munch.restful.client;
 
 import munch.restful.core.exception.OfflineException;
+import munch.restful.core.exception.StructuredException;
 import munch.restful.core.exception.TimeoutException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.http.NoHttpResponseException;
 import org.apache.http.conn.HttpHostConnectException;
 
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Created by: Fuxing
@@ -15,10 +20,45 @@ import java.net.SocketTimeoutException;
  * Project: munch-core
  */
 public final class ExceptionParser {
+    private static final Map<String, Consumer<StructuredException>> mapper = new HashMap<>();
+
+    /**
+     * @param type     to resolve
+     * @param consumer to apply Exception
+     */
+    public static void addConsumer(String type, Consumer<StructuredException> consumer) {
+        mapper.put(type, consumer);
+    }
+
+    /**
+     * @param type     to resolve
+     * @param function to apply Exception
+     * @param <T>      to resolve to
+     */
+    public static <T extends StructuredException> void addResolver(String type, Function<StructuredException, T> function) {
+        addConsumer(type, e -> {
+            throw function.apply(e);
+        });
+    }
+
     public static void parse(Exception e) {
+        if (e instanceof StructuredException) {
+            parseStructured((StructuredException) e);
+        }
+
         for (Throwable throwable : ExceptionUtils.getThrowables(e)) {
             parseEach(e, throwable);
         }
+    }
+
+    /**
+     * @param e structured exception to parse
+     * @throws StructuredException thrown
+     */
+    private static void parseStructured(StructuredException e) throws StructuredException {
+        mapper.get(e.getType()).accept(e);
+
+        throw e;
     }
 
     private static void parseEach(Exception e, Throwable throwable) {
